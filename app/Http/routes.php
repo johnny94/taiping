8616541ca1f71c43b9734912a8ed1a307d3f898e
SzuYaung <johnny123tw@gmail.com>
2015-05-08 15:42:08 +0800

    Initial Test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', ['middleware' => 'auth', 
+	'uses' => 'ClassesController@index']);
 
 Route::get('classes', 'ClassesController@index');
 
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,17 +1,94 @@
 <?php
 
+use App\User;
+
 class ExampleTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$response = $this->call('GET', '/');
 
-		$this->assertEquals(200, $response->getStatusCode());
+	public function userProvider()
+	{
+		$user = new User(['name'=>'Johnny', 
+			'email'=>'johnny@example.com']);
+
+		return array(array($user));
+		//$this->be($user);
+	}
+	
+	public function testAllRouteWithoutLogin()
+	{
+		
+	}
+	
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testHomePageAfterLogin($user)
+	{		
+		$this->be($user);
+
+		$this->call('GET', '/');
+		$this->assertResponseOk();
+	}
+
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testClassesPageAfterLogin($user)
+	{		
+		$this->be($user);
+
+		$this->call('GET', 'classes');
+		$this->assertResponseOk();
+	}
+	
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testCreateLeavePageAfterLogin($user)
+	{		
+		$this->be($user);
+
+		$this->call('GET', 'leaves/create');
+		$this->assertResponseOk();
+	}
+
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testLeavesPageAfterLogin($user)
+	{		
+		$this->be($user);
+
+		$this->call('GET', 'leaves');
+		$this->assertResponseOk();
+	}
+
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testListAllUserWithLeaves($user)
+	{		
+		$this->be($user);
+
+		$this->call('POST', 'leaves/all');
+		$this->assertResponseOk();
+	}
+
+	public function testCreateLeaveStep1()
+	{
+		$this->call('POST', 'leaves');
+		$this->assertResponseOk();
+	}
+
+	/**
+	* @dataProvider userProvider
+	*/
+	public function testClassSwitchingPageAfterLogin($user)
+	{
+		$this->be($user);
+
+		$this->call('GET', 'switchings/1');
+		$this->assertResponseOk();
 	}
 
 }
