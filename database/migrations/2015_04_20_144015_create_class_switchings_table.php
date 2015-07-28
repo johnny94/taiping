6ccd33d8be28430d9284237346a1d6c4ef07a549
Szu Yaung <johnny123tw@gmail.com>
2015-07-28 22:48:31 +0800

    Hotfix:Change name of the classTitie table

--- database/migrations/2015_04_20_144015_create_class_switchings_table.php
+++ database/migrations/2015_04_20_144015_create_class_switchings_table.php
@@ -12,7 +12,7 @@ class CreateClassSwitchingsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('classTitles', function(Blueprint $table)
+		Schema::create('classtitles', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('title');
